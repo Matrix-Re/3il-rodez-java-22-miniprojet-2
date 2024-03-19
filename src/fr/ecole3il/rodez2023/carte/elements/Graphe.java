004c91031b422dd792dfa40faa152c4c49b76267
Anas AMIRI <anas.amiri@soprabanking.com>
2024-03-19 16:18:52 +0100

    Feat : Rédaction des méthod de la class graphe

--- src/fr/ecole3il/rodez2023/carte/elements/Graphe.java
+++ src/fr/ecole3il/rodez2023/carte/elements/Graphe.java
@@ -1,25 +1,32 @@
 package fr.ecole3il.rodez2023.carte.elements;
 
-import java.util.List;
+import java.util.*;
 
 public class Graphe<E> {
-    public void ajouterNoeud(Noeud<E> noeud){
 
+    List <Noeud<E>> listNoeuds = new ArrayList<>();
+    Map<AbstractMap.SimpleEntry<Noeud<E>, Noeud<E>>, Double> aretes = new HashMap<>();
+
+    public void ajouterNoeud(Noeud<E> noeud){
+        if (!listNoeuds.contains(noeud))
+            listNoeuds.add(noeud);
     }
 
     public void ajouterArete(Noeud<E> depart, Noeud<E> arrivee, double cout){
-
+        if (getCoutArete(depart, arrivee) == 0)
+            aretes.put(new AbstractMap.SimpleEntry<Noeud<E>, Noeud<E>>(depart, arrivee), cout);
     }
 
     public double getCoutArete(Noeud<E> depart, Noeud<E> arrivee){
-        return 0;
+        AbstractMap.SimpleEntry<Noeud<E>, Noeud<E>> key = new AbstractMap.SimpleEntry<>(depart, arrivee);
+        return (aretes.containsKey(key)) ? aretes.get(key) : 0;
     }
 
     public List<Noeud<E>> getNoeuds(){
-        return null;
+        return listNoeuds;
     }
 
     public List<Noeud<E>> getVoisins(Noeud<E> noeud){
-        return null;
+        return noeud.getVoisins();
     }
 }
